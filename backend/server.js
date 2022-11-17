c44942950db2f62be4727ec0ebe165c049906ff2
Dung Nguyen <dung34nguyen@gmail.com>
2022-11-17 15:34:04 -0600

    install express and setup mongodb

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+
+//express app 
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+//routes 
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listen on port', process.env.PORT)
+})
